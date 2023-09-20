f4ca0e2a2ba74020dcf6471fd02fa4b1919fc548
Gigil Jacob <gigiljacob93@gmail.com>
2023-09-20 15:23:27 +0530

    Change in the models

--- bio/admin.py
+++ bio/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
-from bio.models import Contact
+from bio.models import Contact, Profile, Resume, SocialProfile
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_on', 'subject')
 
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone')
+
+
+@admin.register(SocialProfile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('linked_in', 'insta', 'skype', 'fb', 'twitter_x')
+
+
+@admin.register(Resume)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'company', 'resume_file')
--- bio/models.py
+++ bio/models.py
@@ -10,3 +10,39 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class SocialProfile(models.Model):
+    linked_in = models.URLField()
+    insta = models.URLField()
+    skype = models.URLField()
+    fb = models.URLField()
+    twitter_x = models.URLField()
+    created_on = models.DateTimeField(auto_created=True, auto_now_add=True)
+
+    def __str__(self):
+        return str(self.created_on)
+
+
+class Resume(models.Model):
+    file_name = models.CharField(max_length=100)
+    company = models.CharField(max_length=100, blank=True)
+    resume_file = models.FileField(upload_to='media/resumes/')
+    created_on = models.DateTimeField(auto_created=True, auto_now_add=True)
+
+    def __str__(self):
+        return str(self.file_name)
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField(blank=True)
+    phone = models.CharField(max_length=13)
+    social_profile = models.ForeignKey(SocialProfile, on_delete=models.CASCADE)
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_created=True, auto_now_add=True)
+
+    def __str__(self):
+        return str(self.email)
